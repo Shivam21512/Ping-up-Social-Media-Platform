9189f40ec597280cab430ac23a60ce288f372c3f
Shivam21512 <ugaleshivam73@gmail.com>
2025-08-10 00:53:51 +0530

    frontend completed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,6 +6,7 @@ import UserProfileInfo from '../components/UserProfileInfo';
 import PostCard from '../components/PostCard';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import ProfileModal from '../components/ProfileModal';
 
 const Profile = () => {
 
@@ -101,7 +102,7 @@ const Profile = () => {
       </div>
 
       {/* Edit Profile Modal */}
-      {showEdit && <p>Show profile edit</p>}
+      {showEdit && <ProfileModal setShowEdit={setShowEdit}/>}
     </div>
   ) : (<Loading/>)
   
