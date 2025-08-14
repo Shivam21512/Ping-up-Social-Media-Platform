a036c3688cf00b1de2681d8131ed19c716450e96
Shivam21512 <ugaleshivam73@gmail.com>
2025-08-15 01:55:23 +0530

    router section update

---
+++ server/controllers/messageController.js
@@ -0,0 +1,10 @@
+
+
+// Create an empty object to store SS Event connections
+const connections = {};
+
+//Controlloer function for the SSE endpoint
+export const sseController = (req,res)=>{
+    const {userId} = req.params
+    console.log('New client connec ')
+}
